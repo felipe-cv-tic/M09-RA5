da7bc9cc4e6684fb28d9b29b2bbe70b0a9c2ac35
Felipe <fcarveg26@gmail.com>
2026-09-17 19:12:47 +0200

    01-Rot13-Locales problems

---
+++ 01-Rot13/src/Rot13.java
@@ -0,0 +1,83 @@
+
+public class Rot13{
+    public final static char[] minuscules = {
+        'a', 'á', 'à', 'b', 'c', 'ç', 'd', 'e', 'é', 'è', 'f', 'g', 'h', 'i', 'í', 'ì', 'ï',
+        'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ó', 'ò', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ù', 'ü', 'v', 'w', 'x', 'y', 'z'
+    };
+    public final static char[] majuscules = {
+        'A', 'Á', 'À', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È', 'F', 'G', 'H', 'I', 'Í', 'Ì', 'Ï',
+        'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'Ó', 'Ò', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ù', 'Ü', 'V', 'W', 'X', 'Y', 'Z'
+    };
+    public static void main(String[] args){
+        System.out.println("Xifrat");
+        System.out.println("---------");
+        System.out.println("ABC                          => "+ xifraRot13("ABC"));
+        System.out.println("XYZ                          => "+ xifraRot13("XYZ"));
+        System.out.println("Hola, Mr. Calçot             => "+ xifraRot13("Hola, Mr. Calçot "));
+        System.out.println("Perdó, per tu què és?        => "+ xifraRot13("Perdó, per tu què és?"));
+
+        System.out.println("Desxifrat");
+        System.out.println("---------");
+        System.out.println("IÏJ                          => "+ desxifraRot13("ABC"));
+        System.out.println("FGH                          => "+ desxifraRot13("XYZ"));
+        System.out.println("Hola, Mr. Calçot             => "+ desxifraRot13("Hola, Mr. Calçot "));
+        System.out.println("Perdó, per tu què és?        => "+ desxifraRot13("Perdó, per tu què és?"));
+        
+    }
+
+    public static String xifraRot13(String cadena){
+        String resultat = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+            if (Character.isUpperCase(c)){
+                for (int j = 0; j < majuscules.length; j++) {
+                    char cArray = majuscules[j];
+                    if (c==cArray){
+                        resultat += majuscules[(j + 13) % majuscules.length];
+                    }
+                }
+            }else if (Character.isLowerCase(c)){
+                for (int j = 0; j < minuscules.length; j++) {
+                    char cArray = minuscules[j];
+                    if (c==cArray){
+                        resultat += minuscules[(j + 13) % minuscules.length];
+                    }
+                }
+            }else{
+                resultat += c;
+            }
+        }
+
+
+        return resultat;
+    }
+
+    public static String desxifraRot13(String cadena){
+        String resultat = "";
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+            if (Character.isUpperCase(c)){
+                for (int j = 0; j < majuscules.length; j++) {
+                    char cArray = majuscules[j];
+                    if (c==cArray){
+                        resultat += majuscules[(j - 13 % majuscules.length + majuscules.length) % majuscules.length];
+                    }
+                }
+            }else if (Character.isLowerCase(c)){
+                for (int j = 0; j < minuscules.length; j++) {
+                    char cArray = minuscules[j];
+                    if (c==cArray){
+                        resultat += minuscules[(j - 13 % minuscules.length + minuscules.length) % minuscules.length];
+                    }
+                }
+            }
+            else{
+                resultat += c;
+            }
+        }
+
+        return resultat;
+    
+    }
+}
